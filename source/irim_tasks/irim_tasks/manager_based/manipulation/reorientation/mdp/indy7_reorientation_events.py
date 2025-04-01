a977d26a61d28f0b21069d9a522b04386ed67500
KJG97 <simonkim590@gmail.com>
2025-04-01 09:43:33 +0900

    Tcube 만들기 전

--- source/irim_tasks/irim_tasks/manager_based/manipulation/reorientation/mdp/indy7_reorientation_events.py
+++ source/irim_tasks/irim_tasks/manager_based/manipulation/reorientation/mdp/indy7_reorientation_events.py
@@ -48,9 +48,6 @@ def randomize_joint_by_gaussian_offset(
     joint_pos_limits = asset.data.soft_joint_pos_limits[env_ids]
     joint_pos = joint_pos.clamp_(joint_pos_limits[..., 0], joint_pos_limits[..., 1])
 
-    # Don't noise the gripper poses
-    joint_pos[:, -2:] = asset.data.default_joint_pos[env_ids, -2:]
-
     # Set into the physics simulation
     asset.set_joint_position_target(joint_pos, env_ids=env_ids)
     asset.set_joint_velocity_target(joint_vel, env_ids=env_ids)
